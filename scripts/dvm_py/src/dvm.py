b06f448b393e83c1affe288624784c6e84e92416
David Poulin <dpoulin@datumtc.ca>
2021-03-16 13:34:25 -0400

    Removed odd quote characters from codebase

--- scripts/dvm_py/src/dvm.py
+++ scripts/dvm_py/src/dvm.py
@@ -2,14 +2,14 @@
 ## Copyright 2021 Datum Technology Corporation
 ## SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
 ## 
-## Licensed under the Solderpad Hardware License v 2.1 (the “License”); you may
+## Licensed under the Solderpad Hardware License v 2.1 (the "License"); you may
 ## not use this file except in compliance with the License, or, at your option,
 ## the Apache License version 2.0. You may obtain a copy of the License at
 ## 
 ##     https://solderpad.org/licenses/SHL-2.1/
 ## 
 ## Unless required by applicable law or agreed to in writing, any work
-## distributed under the License is distributed on an “AS IS” BASIS, WITHOUT
+## distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
 ## WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 ## License for the specific language governing permissions and limitations
 ## under the License.
@@ -45,13 +45,10 @@ vivado_path   = "C:/Xilinx/Vivado/2019.2/bin/"
 uvm_dpi_so    = "uvm_dpi"
 pwd           = os.getcwd()
 project_dir   = pwd + "/.."
-design_path   = project_dir + "/design"
-rtl_path      = design_path + "/rtl"
-rtl_libs_path = rtl_path + "/lib"
-rtl_src_path  = rtl_path + "/src"
+rtl_path      = project_dir + "/rtl"
+rtl_libs_path = rtl_path + "/.imports"
 dv_path       = project_dir + "/dv"
-dv_libs_path  = project_dir + "/../libs/new"
-dv_src_path   = dv_path + "/src"
+dv_libs_path  = dv_path + "/.imports"
 
 
 
@@ -69,14 +66,17 @@ def do_dispatch(args):
     if args['clean']:
         do_clean()
     if args['cmp']:
-        #do_cmp(dv_src_path + "/uvmt_axis_st/uvmt_axis_st.flist", "uvmt_axis_st")
-        do_cmp(dv_src_path + "/uvmt_pkt_snf/uvmt_pkt_snf.flist", "uvmt_axis_st")
+        #do_cmp(dv_path + "/uvmt_axis_st/uvmt_axis_st.flist", "uvmt_axis_st")
+        #do_cmp(dv_path + "/uvmt_pkt_snf/uvmt_pkt_snf.flist", "uvmt_axis_st")
+        do_cmp(dv_path + "/uvmt_clk_st/uvmt_clk_st.flist.xsim", "uvmt_clk_st")
     if args['elab']:
         #do_elab("uvmt_axis_st", "uvmt_axis_st_tb")
-        do_elab("uvmt_pkt_snf", "uvmt_pkt_snf_tb")
+        #do_elab("uvmt_pkt_snf", "uvmt_pkt_snf_tb")
+        do_elab("uvmt_clk_st", "uvmt_clk_st_tb")
     if args['sim']:
         #do_sim("uvmt_axis_st_tb", "uvmt_axis_st_rand_traffic_test", 1, ["SIM_DIR_RESULTS=./"])
-        do_sim("uvmt_pkt_snf_tb", "uvmt_pkt_snf_base_test", 1, ["SIM_DIR_RESULTS=" + pwd + "/"])
+        #do_sim("uvmt_pkt_snf_tb", "uvmt_pkt_snf_base_test", 1, ["SIM_DIR_RESULTS=" + pwd + "/"])
+        do_sim("uvmt_clk_st_tb", "uvmt_clk_st_sanity_test", 1, ["SIM_DIR_RESULTS=" + pwd + "/"])
 
 
 
@@ -85,26 +85,29 @@ def do_paths():
         print("Call to do_paths()")
     
     ### RTL ###
-    set_env_var("RTL_PKT_SNF_PATH", rtl_src_path + "/pkt_snf")
+    set_env_var("RTL_PKT_SNF_PATH", rtl_path + "/pkt_snf")
     
     ### DV ###
     # Libraries
-    set_env_var("UVM_HOME"          , "C:/Users/DavidOuellet-Poulin/Documents/Libraries/1800.2-2017-1.0")
-    set_env_var("DV_UVM_PATH"       , dv_libs_path + "/uvm"            )
-    set_env_var("DV_UVML_HRTBT_PATH", dv_libs_path + "/uvml_hrtbt_v0p1")
-    set_env_var("DV_UVML_TRN_PATH"  , dv_libs_path + "/uvml_trn_v0p1"  )
-    set_env_var("DV_UVML_LOGS_PATH" , dv_libs_path + "/uvml_logs_v0p1" )
-    set_env_var("DV_UVML_SB_PATH"   , dv_libs_path + "/uvml_sb_v0p1"   )
-    set_env_var("DV_UVML_RAL_PATH"  , dv_libs_path + "/uvml_ral_v0p1"  )
-    set_env_var("DV_UVMA_RESET_PATH", dv_libs_path + "/uvma_reset_v0p1")
+    set_env_var("UVM_HOME"           , "C:/Users/DavidOuellet-Poulin/Documents/Libraries/1800.2-2017-1.0")
+    set_env_var("DV_UVM_PATH"        , dv_path + "/uvm"        )
+    set_env_var("DV_UVML_HRTBT_PATH" , dv_path + "/uvml_hrtbt" )
+    set_env_var("DV_UVML_TRN_PATH"   , dv_path + "/uvml_trn"   )
+    set_env_var("DV_UVML_LOGS_PATH"  , dv_path + "/uvml_logs"  )
+    set_env_var("DV_UVML_SB_PATH"    , dv_path + "/uvml_sb"    )
+    set_env_var("DV_UVML_RAL_PATH"   , dv_path + "/uvml_ral"   )
+    set_env_var("DV_UVMA_RESET_PATH" , dv_path + "/uvma_reset" )
+    set_env_var("DV_UVMA_CLK_PATH"   , dv_path + "/uvma_clk"   )
+    set_env_var("DV_UVME_CLK_ST_PATH", dv_path + "/uvme_st_clk")
+    set_env_var("DV_UVMT_CLK_ST_PATH", dv_path + "/uvmt_st_clk")
     
     # Source
-    set_env_var("DV_UVMA_APB_PATH"    , dv_src_path + "/uvma_apb"    )
-    set_env_var("DV_UVMA_AXIS_PATH"   , dv_src_path + "/uvma_axis"   )
-    set_env_var("DV_UVME_PKT_SNF_PATH", dv_src_path + "/uvme_pkt_snf")
-    set_env_var("DV_UVME_AXIS_ST_PATH", dv_src_path + "/uvme_axis_st")
-    set_env_var("DV_UVMT_PKT_SNF_PATH", dv_src_path + "/uvmt_pkt_snf")
-    set_env_var("DV_UVMT_AXIS_ST_PATH", dv_src_path + "/uvmt_axis_st")
+    set_env_var("DV_UVMA_APB_PATH"    , dv_path + "/uvma_apb"    )
+    set_env_var("DV_UVMA_AXIS_PATH"   , dv_path + "/uvma_axis"   )
+    set_env_var("DV_UVME_PKT_SNF_PATH", dv_path + "/uvme_pkt_snf")
+    set_env_var("DV_UVME_AXIS_ST_PATH", dv_path + "/uvme_axis_st")
+    set_env_var("DV_UVMT_PKT_SNF_PATH", dv_path + "/uvmt_pkt_snf")
+    set_env_var("DV_UVMT_AXIS_ST_PATH", dv_path + "/uvmt_axis_st")
 
 
 
